89c5065db6833088f5cda155be872a9b2c6e6350
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-13 07:01:19 -0700

    started testingproject

--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />',()=>{
+  it('renders without crashing', () => {
+    render(<App />)
+  });
+
 });
+describe('hit function tests',()=>{
+  it('clears both balls and strikes when hit event is fired',()=>{
+    const { getByText } = render(<App />);
+    const hit = getByText(/Hit!/i);
+    fireEvent.click(hit);
+    expect(getByText(/Balls/i)).toEqual('Balls: 0');
+  })
+})
